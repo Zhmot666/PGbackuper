c2a52e2491c5a9f3b5969535cfaed14c0ad3436d
Zhmot666 <mailboxforspam@tut.by>
2024-04-23 17:19:00 +0300

    PGB-13

--- data_db.py
+++ data_db.py
@@ -1,11 +1,13 @@
 import sqlite3
 
 
-class Sqlite:
+class DataDB:
     __CONNECTION = sqlite3.connect('db_backuper.db')
     __CREATE_TABLE_SCRIPTS1 = open('SQLScripts/create_TaskList.txt').read()
     __CREATE_TABLE_SCRIPTS2 = open('SQLScripts/create_TaskParameters.txt').read()
     __CREATE_TABLE_SCRIPTS3 = open('SQLScripts/create_Schedule.txt').read()
+    __GET_LIST_OF_TASKS = open('SQLScripts/get_list_of_tasks.txt').read()
+
 
     def __init__(self):
         self.__cursor = self.__CONNECTION.cursor()
@@ -28,4 +30,16 @@ class Sqlite:
         pass
 
     def delete_task(self):
+        pass
+
+    def get_list_of_tasks(self, active_only=True):
+        active = 'Неактивно' if active_only else ''
+        with self.__CONNECTION as self.sql:
+            result = self.sql.execute(self.__GET_LIST_OF_TASKS, (active,))
+            return result.fetchall()
+
+    def get_task_parameters(self):
+        pass
+
+    def get_schedule(self):
         pass
--- ui.py
+++ ui.py
@@ -1,13 +1,24 @@
-from PyQt5 import uic, QtWidgets
+from PyQt5 import uic, QtWidgets, QtGui
 from NewTask import NewTaskDialog
+import data_db
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("ui/MainWindow.ui", self)
+        self.db = data_db.DataDB()
+        self.refresh_list()
 
         self.CreateTaskButton.clicked.connect(self.create_task)
+        self.DeleteTaskButton.clicked.connect(self.delete_task)
+        self.HideDeactivCheckBox.clicked.connect(self.refresh_list)
+
+    def showEvent(self, event):
+        pass
+
+    def delete_task(self):
+        pass
 
     def create_task(self):
         dialog = NewTaskDialog(self)
@@ -15,3 +26,33 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def closeEvent(self, event):
         pass
+
+    def refresh_list(self):
+        task_list = self.db.get_list_of_tasks(self.HideDeactivCheckBox.isChecked())
+        self.TaskTable.clear()
+        self.TaskTable.setColumnCount(5)
+        self.TaskTable.horizontalHeader().setStretchLastSection(True)
+        self.TaskTable.setHorizontalHeaderLabels(["ID", "Название задачи", "Активность", "Расписание",
+                                                  "Командная строка"])
+        self.TaskTable.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.TaskTable.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.TaskTable.setAlternatingRowColors(True)
+        self.TaskTable.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.TaskTable.setShowGrid(True)
+        self.TaskTable.setSortingEnabled(False)
+        # self.TaskTable.setColumnHidden(0, True)
+
+        self.TaskTable.setRowCount(0)
+        for task in task_list:
+            row = self.TaskTable.rowCount()
+            self.TaskTable.setItem(row, 0, QtWidgets.QTableWidgetItem(task[0]))
+            self.TaskTable.setItem(row, 1, QtWidgets.QTableWidgetItem(task[1]))
+            self.TaskTable.setItem(row, 2, QtWidgets.QTableWidgetItem(task[2]))
+            self.TaskTable.insertRow(row)
+            # TODO: не отображается ID. Почему?
+            # TODO: покрасить таблицу
+            item = QtWidgets.QTableWidgetItem()
+            if task[2] == "Активно":
+                item.setForeground(QtGui.QColor("green"))
+            else:
+                item.setForeground(QtGui.QColor("red"))
